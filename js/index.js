1b2705cbf1d125161a3c34e239cbdb1a4681c29e
Paulo Antonio <dev.pauloantonio785@gmail.com>
2022-09-30 10:39:19 -0300

    Criação do módulo

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/**
+ * Trouxe o array do arquivo moduloDados.js
+ * para o index.js
+ */
+ import usuarios from "./moduloDados.js";
+
+console.log(usuarios)
---
+++ js/moduloDados.js
@@ -0,0 +1,47 @@
+/**
+ * Dentro deste arrayexistem usuários com está estrutura:
+ * - id
+ * - nomeUsuario
+ * - senha
+ * - email
+ */
+
+
+const usuarios = [
+    {
+        id: 1,
+        nomeUsuario: "Jose-almir",
+        senha: 12345,
+        email: "jose.almir@soulcodeacademy.com",
+    },
+
+    {
+        id: 2,
+        nomeUsuario: "renato3x",
+        senha: 12345,
+        email: "renato.pereira@gmail.com",
+    },
+
+    {
+        id: 3,
+        nomeUsuario: "bruno-dias",
+        senha: 12345,
+        email: "bruno.dias@gmail.com",
+    },
+
+    {
+        id: 4,
+        nomeUsuario: "pedro-viana",
+        senha: 12345,
+        email: "pedro.viana@gmail.com",
+    },
+]
+
+
+
+export default usuarios;
+
+
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
